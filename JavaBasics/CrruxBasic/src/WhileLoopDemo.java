ce0d2b7b41c2c37a2b4198ae11ae6900aec142d1
Adarsh Singh <adarshsingh2k@gmail.com>
2022-01-05 12:33:09 +0530

    While Loop Done

---
+++ JavaBasics/CrruxBasic/src/WhileLoopDemo.java
@@ -0,0 +1,14 @@
+
+public class WhileLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		while (count < 10) {
+			System.out.println("Loop working");
+			count++;
+			System.out.println(count);
+		}
+	}
+
+}
