9a3eae51323882161104cd1405c232dcc02e2cfd
Adarsh Singh <adarshsingh2k@gmail.com>
2022-01-05 12:04:51 +0530

    If Else done

---
+++ JavaBasics/CrruxBasic/src/IfElse.java
@@ -0,0 +1,13 @@
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		
+		if(a<b) System.out.println("b i s greater than a");
+		else System.out.println("a is greater than b");
+	}
+
+}
